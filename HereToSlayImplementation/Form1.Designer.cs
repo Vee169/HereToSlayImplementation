7f4bc24737d2f9392176d258ad56e2c17b3d21db
naner <naner@VEESPC>
2025-09-09 19:45:31 +0100

    Add project files.

---
+++ HereToSlayImplementation/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace HereToSlayImplementation
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            LoginButton = new Button();
+            UsernameTextBox = new TextBox();
+            PasswordTextBox = new TextBox();
+            SignUpButton = new Button();
+            UsernameLabel = new Label();
+            PasswordLabel = new Label();
+            AccountMessageTextBox = new TextBox();
+            HostButton = new Button();
+            JoinTextBox = new TextBox();
+            JoinButton = new Button();
+            JoinWarningTextBox = new TextBox();
+            SuspendLayout();
+            // 
+            // LoginButton
+            // 
+            LoginButton.BackColor = Color.DimGray;
+            LoginButton.ForeColor = SystemColors.ButtonHighlight;
+            LoginButton.Location = new Point(1199, 98);
+            LoginButton.Name = "LoginButton";
+            LoginButton.Size = new Size(100, 23);
+            LoginButton.TabIndex = 0;
+            LoginButton.Text = "Login";
+            LoginButton.UseVisualStyleBackColor = false;
+            LoginButton.Click += LoginButton_Click;
+            // 
+            // UsernameTextBox
+            // 
+            UsernameTextBox.BackColor = Color.DarkGray;
+            UsernameTextBox.BorderStyle = BorderStyle.None;
+            UsernameTextBox.ForeColor = SystemColors.ButtonHighlight;
+            UsernameTextBox.Location = new Point(1199, 76);
+            UsernameTextBox.Name = "UsernameTextBox";
+            UsernameTextBox.Size = new Size(100, 16);
+            UsernameTextBox.TabIndex = 1;
+            // 
+            // PasswordTextBox
+            // 
+            PasswordTextBox.BackColor = Color.DarkGray;
+            PasswordTextBox.BorderStyle = BorderStyle.None;
+            PasswordTextBox.ForeColor = SystemColors.ButtonHighlight;
+            PasswordTextBox.Location = new Point(1305, 76);
+            PasswordTextBox.Name = "PasswordTextBox";
+            PasswordTextBox.Size = new Size(100, 16);
+            PasswordTextBox.TabIndex = 2;
+            // 
+            // SignUpButton
+            // 
+            SignUpButton.BackColor = Color.DimGray;
+            SignUpButton.ForeColor = SystemColors.ButtonHighlight;
+            SignUpButton.Location = new Point(1305, 98);
+            SignUpButton.Name = "SignUpButton";
+            SignUpButton.Size = new Size(100, 23);
+            SignUpButton.TabIndex = 3;
+            SignUpButton.Text = "Sign Up";
+            SignUpButton.UseVisualStyleBackColor = false;
+            SignUpButton.Click += SignUpButton_Click;
+            // 
+            // UsernameLabel
+            // 
+            UsernameLabel.AutoSize = true;
+            UsernameLabel.ForeColor = SystemColors.ButtonHighlight;
+            UsernameLabel.Location = new Point(1199, 58);
+            UsernameLabel.Name = "UsernameLabel";
+            UsernameLabel.Size = new Size(60, 15);
+            UsernameLabel.TabIndex = 4;
+            UsernameLabel.Text = "Username";
+            // 
+            // PasswordLabel
+            // 
+            PasswordLabel.AutoSize = true;
+            PasswordLabel.ForeColor = SystemColors.ButtonHighlight;
+            PasswordLabel.Location = new Point(1305, 58);
+            PasswordLabel.Name = "PasswordLabel";
+            PasswordLabel.Size = new Size(57, 15);
+            PasswordLabel.TabIndex = 5;
+            PasswordLabel.Text = "Password";
+            // 
+            // AccountMessageTextBox
+            // 
+            AccountMessageTextBox.BackColor = Color.Gray;
+            AccountMessageTextBox.BorderStyle = BorderStyle.None;
+            AccountMessageTextBox.ForeColor = SystemColors.ButtonHighlight;
+            AccountMessageTextBox.Location = new Point(1199, 127);
+            AccountMessageTextBox.Multiline = true;
+            AccountMessageTextBox.Name = "AccountMessageTextBox";
+            AccountMessageTextBox.Size = new Size(206, 41);
+            AccountMessageTextBox.TabIndex = 6;
+            // 
+            // HostButton
+            // 
+            HostButton.BackColor = Color.DimGray;
+            HostButton.Font = new Font("Segoe UI", 20F);
+            HostButton.ForeColor = SystemColors.ButtonHighlight;
+            HostButton.Location = new Point(643, 199);
+            HostButton.Name = "HostButton";
+            HostButton.Size = new Size(186, 72);
+            HostButton.TabIndex = 7;
+            HostButton.Text = "Host";
+            HostButton.UseVisualStyleBackColor = false;
+            HostButton.Click += HostButton_Click;
+            // 
+            // JoinTextBox
+            // 
+            JoinTextBox.BackColor = Color.DarkGray;
+            JoinTextBox.BorderStyle = BorderStyle.None;
+            JoinTextBox.Location = new Point(643, 277);
+            JoinTextBox.Name = "JoinTextBox";
+            JoinTextBox.Size = new Size(186, 16);
+            JoinTextBox.TabIndex = 8;
+            // 
+            // JoinButton
+            // 
+            JoinButton.BackColor = Color.DimGray;
+            JoinButton.Font = new Font("Segoe UI", 20F);
+            JoinButton.ForeColor = SystemColors.ButtonHighlight;
+            JoinButton.Location = new Point(643, 299);
+            JoinButton.Name = "JoinButton";
+            JoinButton.Size = new Size(186, 72);
+            JoinButton.TabIndex = 9;
+            JoinButton.Text = "Join";
+            JoinButton.UseVisualStyleBackColor = false;
+            JoinButton.Click += JoinButton_Click;
+            // 
+            // JoinWarningTextBox
+            // 
+            JoinWarningTextBox.BackColor = Color.Gray;
+            JoinWarningTextBox.BorderStyle = BorderStyle.None;
+            JoinWarningTextBox.Location = new Point(643, 377);
+            JoinWarningTextBox.Name = "JoinWarningTextBox";
+            JoinWarningTextBox.ReadOnly = true;
+            JoinWarningTextBox.Size = new Size(186, 16);
+            JoinWarningTextBox.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Gray;
+            ClientSize = new Size(1472, 650);
+            Controls.Add(JoinWarningTextBox);
+            Controls.Add(JoinButton);
+            Controls.Add(JoinTextBox);
+            Controls.Add(HostButton);
+            Controls.Add(AccountMessageTextBox);
+            Controls.Add(PasswordLabel);
+            Controls.Add(UsernameLabel);
+            Controls.Add(SignUpButton);
+            Controls.Add(PasswordTextBox);
+            Controls.Add(UsernameTextBox);
+            Controls.Add(LoginButton);
+            Name = "Form1";
+            Text = "Login";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button LoginButton;
+        private TextBox UsernameTextBox;
+        private TextBox PasswordTextBox;
+        private Button SignUpButton;
+        private Label UsernameLabel;
+        private Label PasswordLabel;
+        private TextBox AccountMessageTextBox;
+        private Button HostButton;
+        private TextBox JoinTextBox;
+        private Button JoinButton;
+        private TextBox JoinWarningTextBox;
+    }
+}
---
+++ HereToSlayImplementation/Form1.cs
@@ -0,0 +1,246 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
+using Microsoft.Identity.Client;
+using Azure.Core;
+using System.Security.Cryptography.Xml;
+namespace HereToSlayImplementation
+
+{
+    public partial class Form1 : Form
+    {
+        static public Form1 instance;
+        static public SqlConnection sqlConnection;
+        public static string CONNECT = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"C:\\Users\\naner\\source\\repos\\HereToSlayImplementation\\HereToSlayImplementation\\obj\\HereToSlayDatabase.mdf\"; Integrated Security=True;Connect Timeout=30";
+        public bool loggedin = false;
+        public Player player;
+        public string EncryptedPassword;
+        public class Player
+        {
+            private string Username;
+            private int playerID;
+            private int GameID;
+            private Button[] party;
+            private bool player1;
+
+            public Player(string u, int p, int g = -1, bool player1 = false)
+            {
+                Username = u;
+                playerID = p;
+                GameID = g;
+                party = new Button[3];
+                this.player1 = player1;
+            }
+
+            public string GetUsername()
+            {
+                return Username;
+            }
+
+            public int GetplayerID()
+            {
+                return playerID;
+            }
+
+            public int GetGameID()
+            {
+                return GameID;
+            }
+
+            public void SetGameID(int x)
+            {
+                GameID = x;
+            }
+
+            public bool IsPlayer1()
+            {
+                return player1;
+            }
+
+            public void setPlayer1(bool x)
+            {
+                player1 = x;
+            }
+
+            public Button[] GetParty()
+            {
+                return party;
+            }
+
+            public void SetParty(Button[] x)
+            {
+                party = x;
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            sqlConnection = new SqlConnection(CONNECT);
+            instance = this;
+        }
+
+        private void LoginButton_Click(object sender, EventArgs e)
+        {
+            AccountMessageTextBox.Text = string.Empty;
+            UsernameTextBox.Text = UsernameTextBox.Text.Trim();
+            PasswordTextBox.Text = PasswordTextBox.Text.Trim();
+            EncryptedPassword = PasswordTextBox.Text;
+            if (!loggedin)
+            {
+                sqlConnection.Open();
+                SqlCommand command = new SqlCommand($"SELECT UserName, playerID FROM Player WHERE UserName = '{UsernameTextBox.Text}' AND AccountPassword = '{EncryptedPassword}'", sqlConnection);
+
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        loggedin = true;
+                        HostButton.Show();
+                        JoinButton.Show();
+                        JoinTextBox.Show();
+                        player = new Player(UsernameTextBox.Text, reader.GetInt32(1));
+                        UsernameTextBox.Text = PasswordTextBox.Text = "";
+
+                    }
+                    else
+                    {
+                        loggedin = false;
+                        AccountMessageTextBox.Text = "This username password combonation does not exist";
+                        UsernameTextBox.Text = PasswordTextBox.Text = "";
+                        EncryptedPassword = "";
+                    }
+                }
+                sqlConnection.Close();
+            }
+        }
+
+        private void SignUpButton_Click(object sender, EventArgs e)
+        {
+            AccountMessageTextBox.Text = string.Empty;
+            UsernameTextBox.Text = UsernameTextBox.Text.Trim();
+            PasswordTextBox.Text = PasswordTextBox.Text.Trim();
+            EncryptedPassword = PasswordTextBox.Text;
+            if (!loggedin)
+            {
+                if (!(UsernameTextBox.Text.Contains(" ") || PasswordTextBox.Text.Contains(" ")))
+                {
+
+
+                    bool isPlayer = true;
+                    sqlConnection.Open();
+                    SqlCommand command = new SqlCommand($"SELECT UserName FROM Player WHERE UserName = '{UsernameTextBox.Text}'", sqlConnection);
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (!reader.Read())
+                        {
+                            isPlayer = false;
+
+                            loggedin = true;
+                            HostButton.Show();
+                            JoinButton.Show();
+                            JoinTextBox.Show();
+                        }
+                        else
+                        {
+                            AccountMessageTextBox.Text = "That Username already exists";
+                        }
+
+                    }
+                    if (!isPlayer)
+                    {
+                        SqlCommand command2 = new SqlCommand($"INSERT INTO Player (UserName, AccountPassword) VALUES ('{UsernameTextBox.Text}', '{PasswordTextBox.Text}')", sqlConnection);
+                        command2.ExecuteNonQuery();
+
+                        SqlCommand command3 = new SqlCommand($"SELECT UserName, playerID FROM Player WHERE UserName = '{UsernameTextBox.Text}' AND AccountPassword = '{PasswordTextBox.Text}'", sqlConnection);
+
+                        using (SqlDataReader reader = command3.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                player = new Player(UsernameTextBox.Text, reader.GetInt32(1));
+                            }
+                        }
+                    }
+
+                    sqlConnection.Close();
+                }
+                else
+                {
+                    AccountMessageTextBox.Text = "no spaces in the username or password";
+                }
+                UsernameTextBox.Text = PasswordTextBox.Text = "";
+            }
+        }
+
+        private void HostButton_Click(object sender, EventArgs e)
+        {
+            sqlConnection.Open();
+            SqlCommand command = new SqlCommand($"INSERT INTO Games (PlayerID1) VALUES ({player.GetplayerID()})", sqlConnection);
+            command.ExecuteNonQuery();
+
+            SqlCommand command2 = new SqlCommand($"SELECT GameID FROM Games WHERE PlayerID1 = {player.GetplayerID()}", sqlConnection);
+            using (SqlDataReader reader = command2.ExecuteReader())
+            {
+                if (reader.Read())
+                {
+                    player.SetGameID(reader.GetInt32(0));
+                }
+            }
+
+            SqlCommand command3 = new SqlCommand($"UPDATE Player SET GameIDfkp = {player.GetGameID()} WHERE playerID = {player.GetplayerID()}", sqlConnection);
+            command3.ExecuteNonQuery();
+            sqlConnection.Close();
+            player.setPlayer1(true);
+
+            this.Close();
+            new Form2().ShowDialog();
+        }
+
+        private void JoinButton_Click(object sender, EventArgs e)
+        {
+            JoinTextBox.Text.Trim();
+            JoinWarningTextBox.Text = null;
+            bool NotInAGame = true;
+            if (JoinTextBox.Text == null)
+            {
+                JoinWarningTextBox.Text = "you need to input a game code if you want to join a game";
+            }
+            else if (int.TryParse(JoinTextBox.Text, out int value))
+            {
+                sqlConnection.Open();
+
+                SqlCommand command1 = new SqlCommand($"SELECT GameID, PlayerID1 FROM Games WHERE GameID = {value}", sqlConnection);
+                using (SqlDataReader reader = command1.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        if (reader.GetInt32(1) != player.GetplayerID())
+                        {
+                            player.SetGameID(value);
+                        }
+                        else
+                        {
+                            NotInAGame = false;
+                            SqlCommand command = new SqlCommand($"DELETE Games WHERE PlayerID1 = {player.GetplayerID()}", sqlConnection);
+                            JoinWarningTextBox.Text = "Youre account was hosting a game, That game has been terminated please try again";
+                        }
+                    }
+                }
+
+                SqlCommand command2 = new SqlCommand($"UPDATE Games SET PlayerID2 = {player.GetplayerID()} WHERE GameID = {player.GetGameID()} \nUPDATE Player SET GameIDfkp = {player.GetGameID()} WHERE playerID = {player.GetplayerID()}", sqlConnection);
+                command2.ExecuteNonQuery();
+                sqlConnection.Close();
+                this.Close();
+                new Form2().ShowDialog();
+            }
+        }
+    }
+}
